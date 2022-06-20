ad596814024ddc6212b37a00a5dbbfbe8b2a36ad
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-06-20 18:44:28 +0500

    new

--- app/src/main/java/com/example/a621j/MainActivity.java
+++ app/src/main/java/com/example/a621j/MainActivity.java
@@ -2,64 +2,49 @@ package com.example.a621j;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText et_string, et_integer, et_float, et_boolean;
+    EditText et_string, et_integer, et_float;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch _switch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        save();
+    }
+
+    private void save() {
+        Button save = findViewById(R.id.save);
+        save.setOnClickListener(view -> {
+            SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putLong("long", Long.parseLong(et_string.getText().toString()));
+            editor.putInt("int", Integer.parseInt(et_integer.getText().toString()));
+            editor.putFloat("float", Float.parseFloat(et_float.getText().toString()));
+            editor.putBoolean("boolean", _switch.isChecked());
+
+            editor.apply();
+            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void init() {
         et_string = findViewById(R.id.et_string);
         et_integer = findViewById(R.id.et_integer);
         et_float = findViewById(R.id.et_float);
-        et_boolean = findViewById(R.id.et_boolean);
+        _switch = findViewById(R.id._switch);
     }
 
-    public void save(View view) {
-        String name = et_string.getText().toString();
-        int number = Integer.parseInt(et_integer.getText().toString());
-        float floatD = Float.parseFloat(et_float.getText().toString());
-        boolean booleanD = Boolean.parseBoolean(et_boolean.getText().toString());
-        saveString(name);
-        saveInteger(number);
-        saveFloat(floatD);
-        saveBoolean(booleanD);
-    }
-
-    void saveString(String name) {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("name", name);
-        editor.apply();
-    }
-
-    void saveInteger(int number) {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("number", String.valueOf(number));
-        editor.apply();
-    }
-
-    void saveFloat(float floatD) {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("float", String.valueOf(floatD));
-        editor.apply();
-    }
-
-    void saveBoolean(boolean booleanD) {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("boolean", String.valueOf(booleanD));
-        editor.apply();
-    }
 }
